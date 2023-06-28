ee60159131b46f600d17984a394ec4ab1d290412
Pedro Henrique <torres.pedrohen@gmail.com>
2023-06-28 16:12:57 -0300

    feat: update timeout from axios instance

--- src/common/Api.ts
+++ src/common/Api.ts
@@ -5,7 +5,7 @@ const { AUTENTIQUE_TOKEN, AUTENTIQUE_URL } = process.env;
 const Api = (token: string) =>
   axios.create({
     baseURL: AUTENTIQUE_URL || 'https://api.autentique.com.br/v2',
-    timeout: 1000,
+    timeout: 5000,
     headers: {
       Authorization: `Bearer ${token || AUTENTIQUE_TOKEN}`,
     },
